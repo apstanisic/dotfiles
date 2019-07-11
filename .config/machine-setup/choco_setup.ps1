de53594f4eb89ab510bcc3c9dd3dff92e6cdeea2
Aleksandar Stanisic <aleksandar@tuta.io>
2019-07-11 09:18:02 +0200

    Added machine setup folder

---
+++ .config/machine-setup/choco_setup.ps1
@@ -0,0 +1,12 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install -y chromium
+choco install -y vlc
+choco install -y alacritty
+choco install -y vscode
+choco install -y malwarebytes
+choco install -y 7zip
+choco install -y git
+choco install -y qbittorrent
+choco install -y steam
+choco install -y shutup10
